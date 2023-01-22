b7d3f182711243cfa6720efce36f5585d5275f25
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-01-22 23:15:07 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/KWICBOTS/File-sharing-Bot'>Click here</a>\n○ Channel : @kwicbotupdates\n○ Support Group : @kwicbotupdates</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>ᴍɪʏᴜᴋɪ sʜɪʀᴏɢᴧɴᴇ</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ ᴇᴠᴇʀʏᴛʏᴘᴇ ᴍᴏᴠɪᴇs : <a href='https://t.me/everytype_movies'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n○ ᴀɴɪᴍᴇ ɢᴀʟʟᴇʀʏ : @gallery_animes\n○ ᴀɴɪᴍᴇ ᴅɪsᴄᴜssɪᴏɴ : @animee_discussion</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
